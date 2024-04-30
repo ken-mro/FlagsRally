38c594c690caee47fe27d0dc75da05eae9572978
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-04-30 23:00:38 +0900

    Update setting page

--- ViewModels/SettingPageViewModel.cs
+++ ViewModels/SettingPageViewModel.cs
@@ -1,29 +1,42 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CountryData.Standard;
+using FlagsRally.Repository;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace FlagsRally.ViewModels
 {
     public partial class SettingPageViewModel : BaseViewModel
     {
         private CountryHelper _countryHelper;
+        private SettingsPreferences _settingPreferences;
 
         [ObservableProperty]
         ObservableCollection<Country> _countryList;
 
         [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(SelectedCountryCode), nameof(ImageSourceString))]
-        Country? _selectedCountry;
+        [NotifyPropertyChangedFor(nameof(ImageSourceString))]
+        private Country _selectedCountry;
 
-        public string SelectedCountryCode => SelectedCountry?.CountryShortCode ?? "jp";
+        public string ImageSourceString => $"https://flagcdn.com/160x120/{SelectedCountry.CountryShortCode.ToLower()}.png";
 
-        public string ImageSourceString => $"https://www.passportindex.org/countries/{SelectedCountryCode.ToLower()}.png";
-
-        public SettingPageViewModel()
+        public SettingPageViewModel(SettingsPreferences settingPreferences)
         {
+            _settingPreferences = settingPreferences;
+
             _countryHelper = new CountryHelper();
             CountryList = new ObservableCollection<Country>(_countryHelper.GetCountryData());
+
+            _selectedCountry = _countryHelper.GetCountryByCode(_settingPreferences.GetCountryOrRegion());
+
+            PropertyChanged += OnSelectedCountryChanged;
+        }
+
+        private void OnSelectedCountryChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(SelectedCountry))
+            {
+                _settingPreferences.SetCountryOrRegion(SelectedCountry.CountryShortCode);
+            }
         }
     }
 }
