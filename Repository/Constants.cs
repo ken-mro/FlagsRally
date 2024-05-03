b6e6ac4f14f346dc270bd66c4fe7409d8501c510
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-05-04 04:26:01 +0900

    Add Constant Value

---
+++ Repository/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagsRally.Repository;
+public static class Constants
+{
+    private const string DATABSE_NAME = "FlagsRally.db3";
+    public static string DataBasePath => Path.Combine(FileSystem.AppDataDirectory, DATABSE_NAME);
+}
