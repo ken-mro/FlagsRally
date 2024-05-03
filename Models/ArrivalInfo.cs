53df63b4c3574705aedb3e45bff6f84a0e5cead8
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-05-04 04:33:47 +0900

    Add ArrivalInfo and its repositroy class and Interface

---
+++ Models/ArrivalInfo.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using SQLite;
+
+namespace FlagsRally.Models
+{
+    [Table("ArrivalInfo")]
+    public class ArrivalInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; init; }
+
+        public DateTime ArrivalDate { get; init; }
+
+        [MaxLength(2)]
+        public string CountryCode { get; init; }
+        [MaxLength(100)]
+        public string AdminArea { get; init; }
+
+        [MaxLength(250), JsonProperty]
+        public string Placemark { get; init; }
+
+    }
+}
---
+++ Repository/IArrivalInfoRepository.cs
@@ -0,0 +1,16 @@
+﻿using FlagsRally.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagsRally.Repository
+{
+    public interface IArrivalInfoRepository
+    {
+        Task Insert(ArrivalInfo arrivalInfo);
+        Task<List<ArrivalInfo>> GetAll();
+        Task<List<ArrivalInfo>> GetAllByCountryCode(string countryCode);
+    }
+}
