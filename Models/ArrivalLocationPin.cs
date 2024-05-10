1cf31e39b61c09a6e3b719b053cebcf89c09e52f
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-05-10 23:00:36 +0900

    Location: Add location pin class

---
+++ Models/ArrivalLocationPin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagsRally.Models;
+
+public class ArrivalLocationPin
+{
+    public int Id { get; set; }
+    public DateTime ArrivalDate { get; set; }
+    public string ArrivalDateString => ArrivalDate.ToString("dd MMM yyyy", CultureInfo.CreateSpecificCulture("en-US"));
+    public Location PinLocation { get; set; }
+}
