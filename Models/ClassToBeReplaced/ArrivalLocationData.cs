22f484603bbc11d79f5cc9da8126dc37216fe19c
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-05-25 17:59:42 +0900

    Add Alternative database class

---
+++ Models/ClassToBeReplaced/ArrivalLocationData.cs
@@ -0,0 +1,41 @@
+﻿using SQLite;
+
+namespace FlagsRally.Models
+{
+    [Table("ArrivalLocation")]
+    public class ArrivalLocationData
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; init; }
+
+        public DateTime ArrivalDate { get; init; }
+
+        [MaxLength(168)]
+        public string CountryName { get; init; }
+
+        [MaxLength(168)]
+        public string AdminAreaName { get; init; }
+
+        [MaxLength(168)]
+        public string LocalityName { get; init; }
+
+        [MaxLength(168)]
+        public string EnCountryName { get; init; }
+
+        [MaxLength(168)]
+        public string EnAdminAreaName { get; init; }
+
+        [MaxLength(168)]
+        public string EnLocalityName { get; init; }
+
+        [MaxLength(2)]
+        public string CountryCode { get; init; }
+
+        [MaxLength(5)]
+        public string AdminAreaCode { get; init; }
+
+        public double Latitude { get; init; }
+
+        public double Longitude { get; init; }
+    }
+}
