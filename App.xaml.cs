cdac9b833fba4eda273ecd3480faedbcc1bcb847
KenMurao <murao.ken.36x@kyoto-u.jp>
2024-05-27 23:43:46 +0900

    Add function to replace database

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,24 +1,25 @@
-﻿namespace FlagsRally
+﻿using FlagsRally.Repository;
+
+namespace FlagsRally;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public App(IArrivalLocationDataRepository arrivalLocationRepository)
     {
-        public App()
-        {
-            InitializeComponent();
+        InitializeComponent();
+        _ = arrivalLocationRepository.UpdateDatabase();
+        MainPage = new AppShell();
+    }
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = base.CreateWindow(activationState);
 
-            MainPage = new AppShell();
-        }
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            var window = base.CreateWindow(activationState);
+        const int newWidth = 400;
+        const int newHeight = 800;
 
-            const int newWidth = 400;
-            const int newHeight = 800;
+        window.MinimumHeight = window.Height = newHeight;
+        window.MinimumWidth = window.Width = newWidth;
 
-            window.MinimumHeight = window.Height = newHeight;
-            window.MinimumWidth = window.Width = newWidth;
-
-            return window;
-        }
+        return window;
     }
 }
