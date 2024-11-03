1e74388a1fd2a536455e3cff90b6145d40fe6c80
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-11-03 23:31:47 +0900

    Update to be able to show pins on map

--- Models/ArrivalLocationPin.cs
+++ Models/ArrivalLocationPin.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Maui.GoogleMaps;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FlagsRally.Models;
 
-public class ArrivalLocationPin
+public class ArrivalLocationPin : Pin
 {
     public int Id { get; set; }
     public DateTime ArrivalDate { get; set; }
-    public string ArrivalDateString => ArrivalDate.ToString("dd MMM yyyy", CultureInfo.CreateSpecificCulture("en-US"));
-    public Location PinLocation { get; set; }
+
+    public ArrivalLocationPin(int id, DateTime dateTime, Location location)
+        : this(id, dateTime, new Position(location.Latitude, location.Longitude))
+    {
+    }
+
+    public ArrivalLocationPin(int id, DateTime dateTime, Position position)
+    {
+        Id = id;
+        ArrivalDate = dateTime;
+        Label = ArrivalDate.ToString("dd MMM yyyy", CultureInfo.CreateSpecificCulture("en-US"));
+        Position = position;
+        Type = PinType.Place;
+    }
 }
