9137ba4b299cd49247916256037b2cd7c87670b6
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-11-27 00:55:59 +0900

    Update for the android location permission

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,51 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
 
 namespace FlagsRally
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        const int RequestLocationId = 0;
+
+        readonly string[] LocationPermissions =
+        {
+                Manifest.Permission.AccessFineLocation,
+                Manifest.Permission.AccessCoarseLocation
+            };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Check if the location permissions are already granted
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != (int)Permission.Granted)
+            {
+                // Request location permissions
+                ActivityCompat.RequestPermissions(this, LocationPermissions, RequestLocationId);
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (requestCode == RequestLocationId)
+            {
+                if (grantResults[0] == Permission.Granted)
+                {
+                    // Permissions granted
+                }
+                else
+                {
+                    // Permissions denied
+                }
+            }
+        }
     }
 }
