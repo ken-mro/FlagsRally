abb973e6d98cc56467c87b0d96d09725a0460beb
Ken Murao <murao.ken.36x@kyoto-u.jp>
2025-09-13 00:09:55 +0900

    Add FakeLocationException class for error handling
    Introduces the `FakeLocationException` class in the
    `FlagsRally.Exceptions` namespace. This new exception
    class extends the base `Exception` class and provides
    multiple constructors for flexibility, including support
    for serialization.

---
+++ Exceptions/FakeLocationException.cs
@@ -0,0 +1,9 @@
+﻿namespace FlagsRally.Exceptions;
+
+[Serializable]
+public class FakeLocationException : Exception
+{
+    public FakeLocationException() { }
+    public FakeLocationException(string message) : base(message) { }
+    public FakeLocationException(string message, Exception inner) : base(message, inner) { }
+}
